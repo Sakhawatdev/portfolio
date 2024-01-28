4de6c301aa94d3b8e927af53b871e4ba8da2fff1
Sakhawatdev <sakhawathossain326319@gmail.com>
2024-01-28 21:40:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ This is Sakhawat's Portfolio
---
+++ portfolioproject/apps/Myself/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def myself(request):
+    return render(request, 'myself/myself.html')
